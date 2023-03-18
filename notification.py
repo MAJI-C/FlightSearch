dcfcc04ed2676201849356a99ed1de84299e818f
MAJI-C <jermolenko.m@gmail.com>
2023-03-18 18:49:54 +0100

    add html file

---
+++ notification.py
@@ -0,0 +1,4 @@
+# NotificationManager: The NotificationManager class handles 
+# all the notifications sent to passengers, ex regarding the 
+# discovered cheap flights, by delivering notifications to passengers once the flights are found.
+
---
+++ passenger.py
@@ -0,0 +1,2 @@
+# The Passenger class represents an individual who is booking a flight. 
+# It contains information about the passenger's name, age, and contact details, default departure city
